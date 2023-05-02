ba11a98a6c547cac31b9a98db716939cfd784224
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-02 12:44:52 -0600

    Rutas de la aplicación

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
@@ -10,7 +10,6 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
 
 @NgModule({
   declarations: [
-    RegisterPageComponent,
     BasicPageComponent,
     DynamicPageComponent,
     SwitchesPageComponent

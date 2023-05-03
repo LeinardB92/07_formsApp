790d4cbbba89a231f7925b3b24deeb39c99ed851
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2023-05-03 12:58:15 -0600

    establecer y reestablecer valores del formulario, .reset()

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   // Primera forma de crear un formulario reactivo.
   // public myForm: FormGroup =  new FormGroup({
@@ -22,11 +22,18 @@ export class BasicPageComponent {
     inStorage: [0, [Validators.required, Validators.min(0)], []]
   });
 
-  constructor(private fb: FormBuilder){};
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+;
 
   onSave(): void {
     if(this.myForm.invalid) return;
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
   }
 }
